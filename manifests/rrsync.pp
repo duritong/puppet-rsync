29733242bab7122cb1cfdc93b6bae1f79d449e71
mh <mh@immerda.ch>
2010-08-07 02:12:24 +0200

    remove unnecessary fileserver variable (#2460)

--- manifests/rrsync.pp
+++ manifests/rrsync.pp
@@ -4,7 +4,7 @@ class rsync::rrsync {
     include rsync::client
 
     file{'/usr/local/bin/rrsync':
-        source => "puppet://$server/modules/rsync/rrsync/rrsync",
+        source => "puppet:///modules/rsync/rrsync/rrsync",
         require => Package['rsync'],
         owner => root, group => 0, mode => 0755;
     }
